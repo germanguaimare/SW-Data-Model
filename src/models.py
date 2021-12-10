519191a3f4238ffbb462ce3b6f083ceda594bba8
germanguaimare <germanguaimare@gmail.com>
2021-12-10 18:03:41 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -8,23 +8,55 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'User'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(120), nullable=False)
+    email = Column(String(80), nullable=False)
+    password = Column(String(10), nullable=False, unique=True)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorites(Base):
+    __tablename__ = 'Favorites'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer,ForeignKey("User.id"),primary_key=True)
+    charId = Column(String(120), ForeignKey("Characters.id"), nullable=True)
+    vehicleID = Column(String(120), ForeignKey("Vehicles.id"), nullable=True)
+    planetID = Column(String(120), ForeignKey("Planets.id"), nullable=True)
+
+
+class Characters(Base):
+    __tablename__ = 'Characters'
+    # Here we define columns for the table address.
+
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    charName = Column(String(120), nullable=False)
+    charBirthYear = Column(String(15), nullable=True)
+    charGender = Column(String(15), nullable=True)
+    charHairColor = Column(String(15), nullable=True)
+    charEyeColor = Column(String(15), nullable=True)
+
+class Planets(Base):
+    __tablename__ = 'Planets'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    planetName = Column(String(120), nullable=False)
+    charClimate = Column(String(15), nullable=False)
+    charDiameter = Column(Integer, nullable=False)
+    charPopulation = Column(Integer, nullable=True)
+
+class Vehicles(Base):
+    __tablename__ = 'Vehicles'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    vehicleCrew = Column(String(100), nullable=True)
+    charManufacturer = Column(String(15), nullable=True)
+
 
     def to_dict(self):
         return {}
